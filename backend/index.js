76c1c45144feb3d8dfcdd6dcb94c55584da5744b
Mithun-AM <gowdamithu777@gmail.com>
2025-03-05 21:29:28 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,15 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+const mongoURI = process.env.MONGO_URI; // Get from .env
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
 
 app.get("/", (req, res) => res.send("API Running..."));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
