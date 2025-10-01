25e779471e98a233053af5286994fa622e7d9452
Mokshith R D <166110420+Mokshithrd@users.noreply.github.com>
2025-10-02 01:06:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: 'https://smartproctor-invigilator-allocation-3xcm.onrender.com' || 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
