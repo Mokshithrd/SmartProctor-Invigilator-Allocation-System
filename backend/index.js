4ca4c465ea2b6080eb8f13d7d8248dee1097369d
Mokshith R D <166110420+Mokshithrd@users.noreply.github.com>
2025-10-02 01:12:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,27 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:3000", 
+  "https://smartproctor-invigilator-allocation-3xcm.onrender.com"
+];
+
 app.use(cors({
-    origin: 'https://smartproctor-invigilator-allocation-3xcm.onrender.com' || 'http://localhost:3000',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: function(origin, callback) {
+    
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 const userRoute = require("./routes/userRoute");
 const facultyRoute = require("./routes/facultyRoute")
